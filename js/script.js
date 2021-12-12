996af826740815389a353bd4a0bc849cd8f9a297
Tabriz Mammadov <tabrizam@code.edu.azgit config --global user.name TebrizM>
2021-12-12 23:32:42 +0400

    Updated

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $('.next').on('click', function() {
         let currentImg = $('.active');
         let nextImg = currentImg.next();
+        let firstImg = $('div img').first();
 
         if (nextImg.length) {
             let imgId = $(this).attr('data-Id')
@@ -9,6 +10,9 @@ $(document).ready(function() {
             currentImg.removeClass('active').css('z-index', -10);
             nextImg.addClass('active').css('z-index', 10);
 
+        } else if ($("div img").last()) {
+            currentImg.removeClass("active").css("z-index", -10);
+            firstImg.addClass("active").css("z-index", 10);
         }
     });
 
@@ -19,6 +23,9 @@ $(document).ready(function() {
         if (prevImg.length) {
             currentImg.removeClass('active').css('z-index', -10);
             prevImg.addClass('active').css('z-index', 10);
+        } else if ($("div img").first()) {
+            currentImg.removeClass("active").css("z-index", -10);
+            $("div img").last().addClass("active").css("z-index", 10);
         }
     });
 });
